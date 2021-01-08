2139cf2c873024725e8f018e650ab396057b4409
Jaheim Archibald <jaheimarchibald@gmail.com>
2021-01-07 21:46:43 -0500

    First commit

---
+++ login.py
@@ -0,0 +1,127 @@
+from stdiomask import getpass
+import hashlib
+import os
+clear = lambda: os.system('cls')
+
+def main():
+    clear()
+    print("MAIN MENU")
+    print("---------")
+    print()
+    print("1 - Register")
+    print("2 - Login")
+    print()
+    while True:
+        print()
+        userChoice = input("Choose An Option: ")
+        if userChoice in ['1', '2']:
+            break
+    if userChoice == '1':
+        Register()
+    else:
+        Login()
+
+
+
+
+def Register():
+    clear()
+    print("REGISTER")
+    print("--------")
+    print()
+    while True:
+        userName = input("Enter Your Name: ").title()
+        if userName != '':
+            break
+    userName = sanitizeName(userName)
+    while True:
+        userPassword = getpass("Enter Your Password: ")
+        if userPassword != '':
+            break
+    while True:
+        confirmPassword = getpass("Confirm Your Password: ")
+        if confirmPassword == userPassword:
+            break
+        else:
+            print("Passwords Don't Match")
+            print()
+
+    if userAlreadyExist(userName, userPassword):
+        while True:
+            print()
+            error = input("You Are Already Registered.\n\nPress (T) To Try Again:\nPress (L) To Login: ").lower()
+            if error == 't':
+                Register()
+                break
+            elif error == 'l':
+                Login()
+                break
+    addUserInfo([userName, hash_password(userPassword)])
+    
+    print()
+    print("Registered")
+
+def Login():
+    clear()
+    print("LOGIN")
+    print("-----")
+    print()
+    usersInfo = {}
+    with open('userInfo.txt', 'r') as file:
+        for line in file:
+            line = line.split()
+            usersInfo.update({line[0]: line[1]})
+
+    while True:
+        userName = input("Enter Your Name: ").title()
+        userName = '-'.join(userName.split())
+        if userName not in usersInfo:
+            print("You Are Not Registered")
+            print()
+        else:
+            break
+    while True:
+        userPassword = getpass("Enter Your Password: ")
+        if  not check_password_hash(userPassword, usersInfo[userName]):
+            print("Incorrect Password")
+            print()
+        else:
+            break
+
+    print()
+    print("Logged In!")
+
+
+def addUserInfo(userInfo: list):
+    with open('userInfo.txt', 'a') as file:
+        for info in userInfo:
+            file.write(info)
+            file.write(' ')
+        file.write('\n')
+
+def userAlreadyExist(userName, userPassword):
+    userName = '-'.join(userName.split())
+    usersInfo = {}
+    with open('userInfo.txt', 'r') as file:
+        for line in file:
+            line = line.split()
+            if line[0] == userName and line[1] == userPassword:
+                usersInfo.update({line[0]: line[1]})
+    if usersInfo == {}:
+        return False
+    return usersInfo[userName] == userPassword
+
+def sanitizeName(userName):
+    userName = userName.split()
+    userName = '-'.join(userName)
+    return userName
+
+def hash_password(password):
+    return hashlib.sha256(str.encode(password)).hexdigest()
+
+def check_password_hash(password, hash):
+    if hash_password(password) == hash:
+        return True
+    return False
+
+main()
